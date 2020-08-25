9dfcdf7a68235fbcc873726835891802d2e7e02e
Дмитрий Канский <ernokanst@gmail.com>
2020-08-25 15:10:53 +0300

    Добавлен поиск по спискам пользователей

--- GeekBrains SMS/GeekBrains SMS/UserEditor.swift
+++ GeekBrains SMS/GeekBrains SMS/UserEditor.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class UserEditor: UITableViewController {
+class UserEditor: UITableViewController, UISearchBarDelegate {
     let session = Session.instance
-
+    @IBOutlet var searchBar: UISearchBar!
+    var searchActive = false
+    var data: [String] = []
+    var filteredData: [String] = []
+    var loginForNames : [String:String] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        for l in session.loginList {
+            data.append(session.nameData[l]!)
+            filteredData.append(session.nameData[l]!)
+            loginForNames[session.nameData[l]!] = l
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -30,32 +41,52 @@ class UserEditor: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return session.loginList.count
+        return filteredData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "user", for: indexPath)
 
-        cell.textLabel?.text = session.nameData[session.loginList[indexPath.row]]
+        cell.textLabel?.text = filteredData[indexPath.row]
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if self.navigationItem.title == "Изменить пользователя" {
-        session.temp = session.loginList[indexPath.row]
+            session.temp = loginForNames[(self.tableView.cellForRow(at: indexPath)?.textLabel?.text)!]!
         performSegue(withIdentifier: "editUser", sender: nil)
         }
         if self.navigationItem.title == "Удалить пользователя" {
             let alert = UIAlertController(title: "Удаление пользователя", message: "Пользователь \(tableView.cellForRow(at: indexPath)?.textLabel?.text ?? "") будет удалён", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: {action in
-                self.session.loginList.remove(at: indexPath.row)
+                self.session.loginList.remove(at: self.session.loginList.firstIndex(of: (self.tableView.cellForRow(at: indexPath)?.textLabel?.text)!)!)
                 self.performSegue(withIdentifier: "deleteDone", sender: nil)
             }))
             alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+            self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.showsCancelButton = false
+            searchBar.text = ""
+            searchBar.resignFirstResponder()
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
