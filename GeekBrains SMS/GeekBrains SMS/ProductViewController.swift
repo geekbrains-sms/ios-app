245ab1001f35bc9820c2286b35c6dc490ceb117f
Дмитрий Канский <ernokanst@gmail.com>
2020-10-08 15:36:24 +0300

    Добавлена страница товара

---
+++ GeekBrains SMS/GeekBrains SMS/ProductViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProductViewController.swift
+//  GeekBrains SMS
+//
+//  Created by Дмитрий Канский on 08.10.2020.
+//  Copyright © 2020 ernokanst. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+    let session = Session.instance
+    @IBOutlet var name: UIButton!
+    @IBOutlet var category: UIButton!
+    @IBOutlet var desc: UIButton!
+    @IBOutlet var supplier: UILabel!
+    @IBOutlet var quantity: UILabel!
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = session.temp
+        super.viewDidLoad()
+        for p in session.products {
+            if p.product.title == session.temp {
+                self.name.setTitle(p.product.title, for: .normal)
+                self.category.setTitle(p.product.categories[0].title, for: .normal)
+                self.desc.setTitle(p.product.description, for: .normal)
+                self.supplier.text = """
+ Поставщик: ООО "Поставщик"
+ """
+                self.quantity.text = String(p.balance) + " " + p.product.unit.title
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
